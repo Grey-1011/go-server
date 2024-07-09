bc8d7c802b779f13643ebb2097ae7166c1017091
Grey <Grey>
2024-07-09 22:49:32 +0800

    Documentation && /api/chirps?author_id=1

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -35,10 +35,28 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// 初始化 authorID 为 -1（表示没有指定特定作者）。
+	authorID := -1
+	authorIDString := r.URL.Query().Get("author_id")
+	// 如果 author_id 存在
+	if authorIDString != "" {
+		authorID, err = strconv.Atoi(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID")
+			return
+		}
+	}
+
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
+		// 如果指定了 author_id 且 chirp 的 AuthorID 不匹配，跳过该 chirp。
+		if authorID != -1 && dbChirp.AuthorID != authorID {
+			continue
+		}
+
 		chirps = append(chirps, Chirp{
 			ID:   dbChirp.ID,
+			AuthorID:  dbChirp.AuthorID,
 			Body: dbChirp.Body,
 		})
 	}
